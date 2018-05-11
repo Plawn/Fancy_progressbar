d6d826fc79e65bbff2548d21c101754f3b943ff0
Plawn <plawn.yay@gmail.com>
2018-05-11 19:10:27 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
 name='Fancy_progressbar',
-version='0.13',
+version='0.14a0',
 description='An easy to use multi progressbar manager',
 packages = ['Fancy_progressbar'],
-install_requires=[
-                  'threading',
-                  'time'],
 url='https://github.com/Plawn/Fancy_progressbar',
 license='MIT',
 author='Plawn',
